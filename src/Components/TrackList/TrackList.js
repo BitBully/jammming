d128a2c4b0b5fdff4ad0b32b358376adc931fcd0
Andy Anderson <andy@slowlanecafe.com>
2018-02-20 22:48:44 -0500

    Commented out some debugging writes.

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,11 +4,8 @@ import Track from '../Track/Track';
 
 class TrackList extends Component {
     render() {
-        // console.log('\nIn TrackList: ' + this.props.tracks);
-
         // this.props.tracks.map(track => {
         //     console.log('\nIn TrackList.js: \n');
-        //     console.log('  Track ID: ' + track.id + '\n');
         //     console.log('     Track: ' + track.name + '\n');
         //     console.log('    Artist: ' + track.artist + '\n');
         //     console.log('     Album: ' + track.album + '\n\n');
